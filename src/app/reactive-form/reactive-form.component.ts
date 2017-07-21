516bfd02aa10195895ee0f39cc8d1310ef20a987
szuperaz <szuperaz@gmail.com>
2017-07-21 09:44:40 +0200

    initial data binding

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Gender } from '../gender.enum';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+  formGroup: FormGroup;
+  genderOptions = Object.keys(Gender);
 
-  constructor() { }
+  constructor(formBuilder: FormBuilder) {
+    this.initForm(formBuilder);
+  }
+
+  initForm(formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      username: ['zizi', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.formGroup.value);
   }
 
 }
--- src/app/reactive-form/reactive-form.module.ts
+++ src/app/reactive-form/reactive-form.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormComponent } from './reactive-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [ReactiveFormComponent],
   exports: [ReactiveFormComponent]
