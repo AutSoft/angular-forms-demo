2f7ee123555f7fa15a01b6d951400d5e848f2d52
szuperaz <szuperaz@gmail.com>
2017-07-22 09:49:05 +0200

    two way binding

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Gender } from '../gender.enum';
 import { CustomValidators } from 'ng2-validation';
@@ -9,12 +9,20 @@ import { User } from '../user';
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent implements OnChanges {
   @Input() user: User;
+  @Output() userChange = new EventEmitter<User>();
   formGroup: FormGroup;
   genderOptions = Object.keys(Gender);
 
   constructor(private formBuilder: FormBuilder) {
+    this.initForm(this.formBuilder);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user']) {
+      this.formGroup.patchValue(this.user);
+    }
   }
 
   initForm(formBuilder: FormBuilder) {
@@ -24,29 +32,37 @@ export class ReactiveFormComponent implements OnInit {
       password: [null, [Validators.required, Validators.minLength(6)]],
       passwordAgain: null,
       gender: [null, Validators.required],
-      phone: [null, Validators.required]
+      phone: [null, Validators.required],
+      checkbox: false
     });
 
-    this.formGroup.patchValue(this.user);
+    this.formGroup.controls['checkbox'].valueChanges.subscribe(newValue => {
+      const phoneCtrl = this.formGroup.controls['phone'];
+      if (newValue && phoneCtrl.disabled) {
+        phoneCtrl.enable({emitEvent: false});
+      } else if (!newValue && phoneCtrl.enabled) {
+        phoneCtrl.disable({emitEvent: false});
+      }
+    });
+
+    this.formGroup.controls['phone'].valueChanges.subscribe(newValue => {
+      let checkbox = this.formGroup.controls['checkbox'];
+      if (newValue && !checkbox.value) {
+        checkbox.setValue(true);
+      } else if (!newValue && checkbox.value) {
+        checkbox.setValue(false);
+      }
+    });
 
     this.formGroup.controls['passwordAgain'].setValidators(CustomValidators.equalTo(this.formGroup.controls['password']));
-    this.formGroup.controls['phone'].disable();
-  }
-
-  ngOnInit() {
-    this.initForm(this.formBuilder);
   }
 
   submit() {
-    console.log(this.formGroup.value);
-  }
-
-  togglePhoneInputState(event) {
-    if (event.target.checked) {
-      this.formGroup.controls['phone'].enable();
-    } else {
-      this.formGroup.controls['phone'].disable();
-    }
+    const user = this.formGroup.value;
+    delete user.passwordAgain;
+    delete user.checkbox;
+    console.log(user);
+    this.userChange.emit(user);
   }
 
 }
